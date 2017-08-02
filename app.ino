f03ef44e4ec29b6cdf498af7151d0f4c289fe683
kwokhung <chu_kwokhung@yahoo.com.hk>
2017-08-02 16:01:07 +0800

    init

--- app.ino
+++ app.ino
@@ -1,78 +1,15 @@
-/*
-  ASCII table
+// ESP32 Touch Test
+// Just test touch pin - Touch0 is T0 which is on GPIO 4.
 
- Prints out byte values in all possible formats:
- * as raw binary values
- * as ASCII-encoded decimal, hex, octal, and binary values
-
- For more on ASCII, see http://www.asciitable.com and http://en.wikipedia.org/wiki/ASCII
-
- The circuit:  No external hardware needed.
-
- created 2006
- by Nicholas Zambetti
- modified 9 Apr 2012
- by Tom Igoe
-
- This example code is in the public domain.
-
- <http://www.zambetti.com>
-
- */
-void setup() {
-  //Initialize serial and wait for port to open:
+void setup()
+{
   Serial.begin(115200);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-
-  // prints title with ending line break
-  Serial.println("ASCII Table ~ Character Map");
+  delay(1000); // give me time to bring up serial monitor
+  Serial.println("ESP32 Touch Test");
 }
 
-// first visible ASCIIcharacter '!' is number 33:
-int thisByte = 33;
-// you can also write ASCII characters in single quotes.
-// for example. '!' is the same as 33, so you could also use this:
-//int thisByte = '!';
-
-void loop() {
-  // prints value unaltered, i.e. the raw binary version of the
-  // byte. The serial monitor interprets all bytes as
-  // ASCII, so 33, the first number,  will show up as '!'
-  Serial.write(thisByte);
-
-  Serial.print(", dec: ");
-  // prints value as string as an ASCII-encoded decimal (base 10).
-  // Decimal is the  default format for Serial.print() and Serial.println(),
-  // so no modifier is needed:
-  Serial.print(thisByte);
-  // But you can declare the modifier for decimal if you want to.
-  //this also works if you uncomment it:
-
-  // Serial.print(thisByte, DEC);
-
-
-  Serial.print(", hex: ");
-  // prints value as string in hexadecimal (base 16):
-  Serial.print(thisByte, HEX);
-
-  Serial.print(", oct: ");
-  // prints value as string in octal (base 8);
-  Serial.print(thisByte, OCT);
-
-  Serial.print(", bin: ");
-  // prints value as string in binary (base 2)
-  // also prints ending line break:
-  Serial.println(thisByte, BIN);
-
-  // if printed last visible character '~' or 126, stop:
-  if (thisByte == 126) {    // you could also use if (thisByte == '~') {
-    // This loop loops forever and does nothing
-    while (true) {
-      continue;
-    }
-  }
-  // go on to the next character
-  thisByte++;
+void loop()
+{
+  Serial.println(touchRead(T0));  // get value using T0
+  delay(1000);
 }
