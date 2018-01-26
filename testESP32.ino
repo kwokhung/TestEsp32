c3607f57756b2e186fe935a219b47e09d45fd690
kwokhung <chu_kwokhung@yahoo.com.hk>
2018-01-26 09:59:50 +0800

    init

--- testESP32.ino
+++ testESP32.ino
@@ -30,8 +30,8 @@ uint8_t value = 0;
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
-#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define SERVICE_UUID "ff10"
+#define CHARACTERISTIC_UUID "ff12"
 
 class MyServerCallbacks : public BLEServerCallbacks
 {
