c45ddc7ba71e818061247adf99dd6e3f5fa5a08e
kwokhung <chu_kwokhung@yahoo.com.hk>
2018-02-02 17:19:15 +0800

    init

--- testESP32.ino
+++ testESP32.ino
@@ -1,6 +1,7 @@
 #include "Ble.h"
 
-Ble ble("Thermometer", "4fafc201-1fb5-459e-8fcc-c5c9c331914b", "beb5483e-36e1-4688-b7f5-ea07361b26a8");
+//Ble ble("Thermometer", "4fafc201-1fb5-459e-8fcc-c5c9c331914b", "beb5483e-36e1-4688-b7f5-ea07361b26a8");
+Ble *ble = new Ble("Thermometer", "4fafc201-1fb5-459e-8fcc-c5c9c331914b", "beb5483e-36e1-4688-b7f5-ea07361b26a8");
 
 void setup()
 {
@@ -9,7 +10,8 @@ void setup()
     uint32_t delayTime = 1000;
 
     xTaskCreate(iAmHereTask, "I Am Here", 10000, &delayTime, 1, NULL);
-    xTaskCreate(Ble::startUp, "BLE", 10000, &ble, 1, NULL);
+    //xTaskCreate(Ble::startUp, "BLE", 10000, &ble, 1, NULL);
+    xTaskCreate(Ble::startUp, "BLE", 10000, ble, 1, NULL);
 }
 
 void loop()
