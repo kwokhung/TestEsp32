b6e5357e7ff8b99e00b3e457875c6b5e9a0a9a28
kwokhung <chu_kwokhung@yahoo.com.hk>
2018-02-06 09:58:53 +0800

    init

--- testESP32.ino
+++ testESP32.ino
@@ -1,15 +1,10 @@
-#include <esp_freertos_hooks.h>
-
 #include "Ble.h"
 
 Ble *ble = new Ble("Thermometer", "4fafc201-1fb5-459e-8fcc-c5c9c331914b", "beb5483e-36e1-4688-b7f5-ea07361b26a8");
-int counter = 0;
 
 void setup()
 {
     Serial.begin(115200);
-
-    esp_register_freertos_idle_hook(vApplicationIdleHook);
 
     uint32_t delayTime = 1000;
 
@@ -29,16 +24,6 @@ void setup()
     },
                 "I Am Here", 10000, &delayTime, 1, NULL);
 
-    char task1Param[12] = "task1Param";
-
-    xTaskCreate(
-        task1,              /* Task function. */
-        "task1",            /* name of task. */
-        10000,              /* Stack size of task */
-        (void *)task1Param, /* parameter of the task */
-        1,                  /* priority of the task */
-        NULL);
-
     xTaskCreate(Ble::startUp, "BLE", 10000, ble, 1, &ble->task);
 
     //vTaskStartScheduler();
@@ -46,29 +31,4 @@ void setup()
 
 void loop()
 {
-}
-
-void task1(void *parameter)
-{
-    Serial.println((char *)parameter);
-    /* loop forever */
-    for (;;)
-    {
-        Serial.print("task1 is running and counter : ");
-        Serial.println(counter);
-        if (counter > 30000)
-        {
-            counter = 0;
-        }
-        /* block this task so that Idle task has chance to run */
-        delay(10);
-    }
-    /* delete a task when finish, 
-  this will never happen because this is infinity loop */
-    vTaskDelete(NULL);
-}
-
-bool vApplicationIdleHook(void)
-{
-    counter++;
 }
