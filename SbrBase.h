2a2926190cde4b9a6440cf4574d82a2c458615e7
kwokhung <chu_kwokhung@yahoo.com.hk>
2018-02-12 14:11:47 +0800

    init

---
+++ SbrBase.h
@@ -0,0 +1,67 @@
+#include <string>
+#include <esp_task_wdt.h>
+
+template <typename T>
+class SbrBase
+{
+  public:
+    virtual void setup();
+    virtual void loop();
+
+    static T *getSingleTon(std::string name);
+    static void startUp(void *parameter);
+
+    std::string name;
+    TaskHandle_t task;
+
+  protected:
+    SbrBase(std::string name);
+
+    static T *singleTon;
+};
+
+template <typename T>
+SbrBase<T>::SbrBase(std::string name)
+    : name(name)
+{
+    task = NULL;
+}
+
+template <typename T>
+void SbrBase<T>::setup(){};
+
+template <typename T>
+void SbrBase<T>::loop(){};
+
+template <typename T>
+T *SbrBase<T>::getSingleTon(std::string name)
+{
+    if (singleTon == NULL)
+    {
+        singleTon = new T(name);
+    }
+
+    return (singleTon);
+}
+
+template <typename T>
+void SbrBase<T>::startUp(void *parameter)
+{
+    T *sbrBase = (T *)parameter;
+
+    sbrBase->setup();
+
+    esp_task_wdt_add(sbrBase->task);
+
+    while (true)
+    {
+        sbrBase->loop();
+
+        esp_task_wdt_reset();
+    }
+
+    vTaskDelete(NULL);
+}
+
+template <typename T>
+T *SbrBase<T>::singleTon = NULL;
--- testESP32.ino
+++ testESP32.ino
@@ -1,65 +1,60 @@
-#include <time.h>
-#include <sys/time.h>
+#include "Ble.h"
 
-#include "SbrMpu.h"
-#include "SbrMotor.h"
-#include "SbrControl.h"
-#include "SbrPid.h"
-#include "SbrConfig.h"
+#include "SbrBase.h"
 
-SbrMpu *sbrMpu = SbrMpu::getSingleTon("SBR - MPU");
-SbrMotor *sbrMotor = SbrMotor::getSingleTon("SBR - Motor");
-SbrControl *sbrControl = SbrControl::getSingleTon("SBR - Control");
-SbrPid *sbrPid = SbrPid::getSingleTon("SBR - PID");
-SbrConfig *sbrConfig = SbrConfig::getSingleTon("SBR - Config");
+class SbrXXX : public SbrBase<SbrXXX>
+{
+public:
+  SbrXXX(std::string name)
+      : SbrBase(name)
+  {
+  }
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-uint8_t temprature_sens_read();
-#ifdef __cplusplus
+  void setup() override;
+  void loop() override;
+
+private:
+};
+
+void SbrXXX::setup()
+{
 }
-#endif
 
-uint8_t temp_farenheit;
-float temp_celsius;
-char strftime_buf[64];
-time_t now = 0;
-struct tm timeinfo;
-char buf[256];
+void SbrXXX::loop()
+{
+}
+
+SbrXXX *sbrXXX = SbrXXX::getSingleTon("SBR - XXX");
+
+Ble *ble = new Ble("Thermometer", "4fafc201-1fb5-459e-8fcc-c5c9c331914b", "beb5483e-36e1-4688-b7f5-ea07361b26a8");
 
 void setup()
 {
   Serial.begin(115200);
-  tzset();
 
-  SbrPid::sbrMpu = sbrMpu;
-  SbrPid::sbrMotor = sbrMotor;
+  uint32_t delayTime = 1000;
 
-  SbrConfig::sbrMotor = sbrMotor;
-  SbrConfig::sbrPid = sbrPid;
+  xTaskCreate([&](void *parameter) {
+    uint32_t delayTime = *(uint32_t *)parameter;
 
-  xTaskCreate(SbrMpu::startUp, sbrMpu->name.c_str(), 10000, sbrMpu, 1, &sbrMpu->task);
-  xTaskCreate(SbrMotor::startUp, sbrMotor->name.c_str(), 10000, sbrMotor, 1, &sbrMotor->task);
-  xTaskCreate(SbrControl::startUp, sbrControl->name.c_str(), 10000, sbrControl, 2, &sbrControl->task);
-  xTaskCreate(SbrPid::startUp, sbrPid->name.c_str(), 10000, sbrPid, 2, &sbrPid->task);
-  xTaskCreate(SbrConfig::startUp, sbrConfig->name.c_str(), 10000, sbrConfig, 1, &sbrConfig->task);
+    Serial.printf("Parameter: %d\n", delayTime);
+
+    for (;;)
+    {
+      Serial.println("I am here.");
+
+      vTaskDelay(pdMS_TO_TICKS(delayTime));
+    }
+
+    vTaskDelete(NULL);
+  },
+              "I Am Here", 10000, &delayTime, 1, NULL);
+
+  xTaskCreate(Ble::startUp, "BLE", 10000, ble, 1, &ble->task);
+
+  //vTaskStartScheduler();
 }
 
 void loop()
 {
-  localtime_r(&now, &timeinfo);
-  strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
-  sprintf(buf, "scan start %02d:%02d:%02d ", timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
-  Serial.print(buf);
-
-  temp_farenheit = temprature_sens_read();
-  temp_celsius = (temp_farenheit - 32) / 1.8;
-  Serial.print("Temp onBoard ");
-  Serial.print(temp_farenheit);
-  Serial.print("°F ");
-  Serial.print(temp_celsius);
-  Serial.println("°C");
-  delay(60000);
-  now++;
 }
