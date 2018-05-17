3b57bd724c22ca31cfb28120b96090eb61b713d7
kwokhung <chu_kwokhung@yahoo.com.hk>
2018-05-17 11:25:45 +0800

    init

--- testESP32.ino
+++ testESP32.ino
@@ -1,77 +1,81 @@
-#include <stdio.h>
-#include <FreeRTOS.h>
-#include <esp_task_wdt.h>
-#include <Wire.h>
-
-#define DATA_LENGTH 512  /*!<Data buffer length for test buffer*/
-#define RW_TEST_LENGTH 4 /*!<Data length for r/w test, any value from 0-DATA_LENGTH*/
-
-#define ESP_SLAVE_ADDR 0x28 /*!< ESP32 slave address, you can set any 7bit value */
-
-static void i2c_example_master_init()
-{
-  Wire.begin();
-}
-
-static void i2c_example_master_write_slave(uint8_t *data_wr, size_t size)
-{
-  Wire.beginTransmission(ESP_SLAVE_ADDR);
-  Wire.write(data_wr, size);
-  Wire.endTransmission(true);
-}
-
-static void disp_buf(uint8_t *buf, int len)
-{
-  int i;
-
-  for (i = 0; i < len; i++)
-  {
-    printf("%02x ", buf[i]);
-
-    if ((i + 1) % 16 == 0)
-    {
-      printf("\n");
-    }
-  }
-
-  printf("\n");
-}
+//#include "SbrXXX.h"
+//#include "SbrXXX01.h"
+//#include "SbrXXX02.h"
+//#include "SbrXXX03.h"
+//#include "SbrXXX04.h"
+//#include "SbrXXX05.h"
+//#include "SbrXXX06.h"
+//#include "SbrXXX07.hpp"
+//#include "SbrXXX08.hpp"
+//#include "SbrXXX09.hpp"
+//#include "SbrXXX10.hpp"
+//#include "SbrXXX11.hpp"
+#include "SbrXXX12.hpp"
+//#include "SbrYYY.h"
+//#include "SbrZZZ.h"
+//#include "SbrMpu.h"
+//#include "SbrMotor.h"
+//#include "SbrControl.h"
+//#include "SbrPid.h"
+//#include "SbrConfig.h"
 
 void setup()
 {
   Serial.begin(115200);
 
-  i2c_example_master_init();
+  //SbrXXX *sbrXXX = SbrXXX::getSingleTon("SBR - XXX");
+  //SbrXXX01 *sbrXXX01 = SbrXXX01::getSingleTon("SBR - XXX01");
+  //SbrXXX02 *sbrXXX02 = SbrXXX02::getSingleTon("SBR - XXX02");
+  //SbrXXX03 *sbrXXX03 = SbrXXX03::getSingleTon("SBR - XXX03");
+  //SbrXXX04 *sbrXXX04 = SbrXXX04::getSingleTon("SBR - XXX04");
+  //SbrXXX05 *sbrXXX05 = SbrXXX05::getSingleTon("SBR - XXX05");
+  //SbrXXX06 *sbrXXX06 = SbrXXX06::getSingleTon("SBR - XXX06");
+  //SbrXXX07 *sbrXXX07 = SbrXXX07::getSingleTon("SBR - XXX07");
+  //SbrXXX08 *sbrXXX08 = SbrXXX08::getSingleTon("SBR - XXX08");
+  //SbrXXX09 *sbrXXX09 = SbrXXX09::getSingleTon("SBR - XXX09");
+  //SbrXXX10 *sbrXXX10 = SbrXXX10::getSingleTon("SBR - XXX10");
+  //SbrXXX11 *sbrXXX11 = SbrXXX11::getSingleTon("SBR - XXX11");
+  SbrXXX12 *sbrXXX12 = SbrXXX12::getSingleTon("SBR - XXX12");
+  //SbrYYY *sbrYYY = SbrYYY::getSingleTon("SBR - YYY");
+  //SbrZZZ *sbrZZZ = SbrZZZ::getSingleTon("SBR - ZZZ");
+  //SbrMpu *sbrMpu = SbrMpu::getSingleTon("SBR - MPU");
+  //SbrMotor *sbrMotor = SbrMotor::getSingleTon("SBR - Motor");
+  //SbrControl *sbrControl = SbrControl::getSingleTon("SBR - Control");
+  //SbrPid *sbrPid = SbrPid::getSingleTon("SBR - PID");
+  //SbrConfig *sbrConfig = SbrConfig::getSingleTon("SBR - Config");
 
-  Serial.println("i2c_example_master_init");
+  //SbrZZZ::sbrXXX = sbrXXX;
+
+  //SbrPid::sbrMpu = sbrMpu;
+  //SbrPid::sbrMotor = sbrMotor;
+
+  //SbrConfig::sbrMotor = sbrMotor;
+  //SbrConfig::sbrPid = sbrPid;
+
+  //SbrXXX10::sbrXXX11 = sbrXXX11;
+
+  //xTaskCreate(SbrXXX::startUp, sbrXXX->name.c_str(), 10000, sbrXXX, 1, &sbrXXX->task);
+  //xTaskCreate(SbrXXX01::startUp, sbrXXX01->name.c_str(), 10000, sbrXXX01, 1, &sbrXXX01->task);
+  //xTaskCreate(SbrXXX02::startUp, sbrXXX02->name.c_str(), 10000, sbrXXX02, 1, &sbrXXX02->task);
+  //xTaskCreate(SbrXXX03::startUp, sbrXXX03->name.c_str(), 10000, sbrXXX03, 1, &sbrXXX03->task);
+  //xTaskCreate(SbrXXX04::startUp, sbrXXX04->name.c_str(), 10000, sbrXXX04, 1, &sbrXXX04->task);
+  //xTaskCreate(SbrXXX05::startUp, sbrXXX05->name.c_str(), 10000, sbrXXX05, 1, &sbrXXX05->task);
+  //xTaskCreate(SbrXXX06::startUp, sbrXXX06->name.c_str(), 10000, sbrXXX06, 1, &sbrXXX06->task);
+  //xTaskCreate(SbrXXX07::startUp, sbrXXX07->name.c_str(), 10000, sbrXXX07, 1, &sbrXXX07->task);
+  //xTaskCreate(SbrXXX08::startUp, sbrXXX08->name.c_str(), 10000, sbrXXX08, 1, &sbrXXX08->task);
+  //xTaskCreate(SbrXXX09::startUp, sbrXXX09->name.c_str(), 10000, sbrXXX09, 1, &sbrXXX09->task);
+  //xTaskCreate(SbrXXX10::startUp, sbrXXX10->name.c_str(), 10000, sbrXXX10, 1, &sbrXXX10->task);
+  //xTaskCreate(SbrXXX11::startUp, sbrXXX11->name.c_str(), 10000, sbrXXX11, 2, &sbrXXX11->task);
+  xTaskCreate(SbrXXX12::startUp, sbrXXX12->name.c_str(), 10000, sbrXXX12, 2, &sbrXXX12->task);
+  //xTaskCreate(SbrYYY::startUp, sbrYYY->name.c_str(), 10000, sbrYYY, 1, &sbrYYY->task);
+  //xTaskCreate(SbrZZZ::startUp, sbrZZZ->name.c_str(), 10000, sbrZZZ, 1, &sbrZZZ->task);
+  //xTaskCreate(SbrMpu::startUp, sbrMpu->name.c_str(), 10000, sbrMpu, 1, &sbrMpu->task);
+  //xTaskCreate(SbrMotor::startUp, sbrMotor->name.c_str(), 10000, sbrMotor, 1, &sbrMotor->task);
+  //xTaskCreate(SbrControl::startUp, sbrControl->name.c_str(), 10000, sbrControl, 2, &sbrControl->task);
+  //xTaskCreate(SbrPid::startUp, sbrPid->name.c_str(), 10000, sbrPid, 2, &sbrPid->task);
+  //xTaskCreate(SbrConfig::startUp, sbrConfig->name.c_str(), 10000, sbrConfig, 2, &sbrConfig->task);
 }
 
 void loop()
 {
-  int ret;
-  uint8_t *data_wr = (uint8_t *)malloc(DATA_LENGTH);
-
-  data_wr[0] = 0x00;
-  data_wr[1] = 0x01;
-  data_wr[2] = 0x01;
-  data_wr[3] = 0x00;
-
-  while (true)
-  {
-    i2c_example_master_write_slave(data_wr, RW_TEST_LENGTH);
-
-    printf("*******************\n");
-    printf("MASTER WRITE TO SLAVE\n");
-    printf("*******************\n");
-    printf("----Master write ----\n");
-
-    disp_buf(data_wr, RW_TEST_LENGTH);
-
-    for (int i = 0; i < 1; i++)
-    {
-      esp_task_wdt_reset();
-
-      delay(1000);
-    }
-  }
 }
